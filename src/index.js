6845691f6d0f4ebe9dc5c5cf755f3a2ec3e9b6ac
Cristian Dias <cristianhernan.dias@gmail.com>
2019-02-28 14:05:50 -0300

    feat(src): add export credentials

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Logs from './components/Logs';
 import Users from './components/Users/UsersList';
+import Credentials from './components/Users/Credentials';
 
 export {Logs};
 export {Users};
+export {Credentials};
