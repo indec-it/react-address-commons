00321a2e81a60c5a222c3852d8a886124e85e56f
Maximiliano Forlenza <forlenzamaximiliano@gmail.com>
2019-03-09 11:07:55 -0300

    feat(downloadBlock): add service to download block in csv

---
+++ src/services/index.js
@@ -0,0 +1,9 @@
+import DownloadBlock from './downloadBlock';
+import Log from './log';
+import Review from './review';
+import User from './user';
+
+export {DownloadBlock};
+export {Log};
+export {Review};
+export {User};
