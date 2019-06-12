4b65303f2252533860e08f210e39ea1dcf9e0ea0
juanlopez1 <juan1manuel9lopez96@gmail.com>
2019-06-12 11:06:12 -0300

    fix(logs): search by proflie state when user isn t national coordinator

--- src/sagas/session.js
+++ src/sagas/session.js
@@ -3,14 +3,15 @@ import {call, put} from 'redux-saga/effects';
 
 import UserService from '../services/user';
 import {handleError, receiveSession} from '../actions';
+import {isNationalCoordinator} from '../util';
 
 export function* fetchSession() {
     try {
         const profile = yield call(UserService.profile);
         if (profile.error) {
-            yield put(handleError({handleError: true, errorMsg: profile.error}));
+            yield put(handleError(profile.error));
         } else {
-            yield put(receiveSession(profile));
+            yield put(receiveSession({...profile, isNationalCoordinator: isNationalCoordinator(profile)}));
         }
     } catch (err) {
         yield put(receiveSession(null));
