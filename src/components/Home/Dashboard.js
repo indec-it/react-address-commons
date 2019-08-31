aafe20106881f5021793457e2f29d127aabaed52
Maximiliano Forlenza <forlenzamaximiliano@gmail.com>
2019-08-31 16:29:00 -0300

    refactor(dashboard): remove loadingIndicator

--- src/components/Home/Dashboard.js
+++ src/components/Home/Dashboard.js
@@ -1,8 +1,7 @@
-import React, {PureComponent, Fragment} from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {Row, Col} from 'react-bootstrap';
-import {LoadingIndicator} from '@indec/react-commons';
 
 import {MonitoringGraphics, Map} from '../Charts';
 import {
@@ -11,38 +10,8 @@ import {
 import {requestFetchLogsByState} from '../../actions/overview';
 
 class Dashboard extends PureComponent {
-    static propTypes = {
-        fetchOverview: PropTypes.func.isRequired,
-        cleanOverviewReducer: PropTypes.func.isRequired,
-        setMapState: PropTypes.func.isRequired,
-        cleanMapSelection: PropTypes.func.isRequired,
-        requestFetchLogsByState: PropTypes.func.isRequired,
-        selectedState: PropTypes.shape({
-            state: PropTypes.number,
-            name: PropTypes.string
-        }).isRequired,
-        availableStates: PropTypes.arrayOf(PropTypes.shape({})),
-        general: PropTypes.arrayOf(PropTypes.shape({})),
-        response: PropTypes.arrayOf(PropTypes.shape({})),
-        users: PropTypes.arrayOf(PropTypes.shape({})),
-        profile: PropTypes.shape({
-            state: PropTypes.number,
-            roles: PropTypes.arrayOf(PropTypes.string)
-        }),
-        loading: PropTypes.bool
-    };
-
-    static defaultProps = {
-        availableStates: null,
-        profile: null,
-        general: null,
-        response: null,
-        users: null,
-        loading: false
-    };
-
     componentDidMount() {
-        this.props.fetchOverview(this.props.profile);
+        this.props.fetchOverview();
     }
 
     componentWillUnmount() {
@@ -54,12 +23,10 @@ class Dashboard extends PureComponent {
         this.props.requestFetchLogsByState(state);
     }
 
-    renderContent() {
-        const {
-            availableStates, general, response, users, profile, selectedState
-        } = this.props;
+    render() {
+        const {availableStates, selectedState} = this.props;
         return (
-            <Fragment>
+            <Row>
                 <Col sm={3} className="col-map">
                     <Map
                         availableStates={availableStates}
@@ -69,39 +36,36 @@ class Dashboard extends PureComponent {
                     />
                 </Col>
                 <Col sm={9} className="no-padding">
-                    <MonitoringGraphics
-                        province={general}
-                        state={selectedState.state}
-                        stateName={selectedState.name}
-                        {...{response, users}}
-                        roles={profile.roles}
-                    />
+                    <MonitoringGraphics/>
                 </Col>
-            </Fragment>
-        );
-    }
-
-    render() {
-        const {general, loading} = this.props;
-        return (
-            <Row>
-                {!loading && general ? this.renderContent() : <LoadingIndicator label="Obteniendo Información..."/>}
             </Row>
         );
     }
 }
 
+Dashboard.propTypes = {
+    fetchOverview: PropTypes.func.isRequired,
+    cleanOverviewReducer: PropTypes.func.isRequired,
+    setMapState: PropTypes.func.isRequired,
+    cleanMapSelection: PropTypes.func.isRequired,
+    requestFetchLogsByState: PropTypes.func.isRequired,
+    selectedState: PropTypes.shape({
+        state: PropTypes.number,
+        name: PropTypes.string
+    }),
+    availableStates: PropTypes.arrayOf(PropTypes.shape({}))
+};
+
+Dashboard.defaultProps = {
+    availableStates: [],
+    selectedState: {}
+};
+
 export default connect(
     state => ({
-        general: state.overview.general,
-        response: state.overview.response,
-        users: state.overview.users,
         selectedState: state.overview.selectedState,
         availableStates: state.overview.availableStates,
-        stateName: state.overview.stateName,
-        loading: state.overview.loading,
-        isNationalCoordinator: state.overview.isNationalCoordinator,
-        profile: state.session.profile
+        isNationalCoordinator: state.overview.isNationalCoordinator
     }),
     {
         fetchOverview, cleanOverviewReducer, setMapState, cleanMapSelection, requestFetchLogsByState
