d2c6737c833d3acf22a6146f6a3b89000572d424
Maximiliano Forlenza <forlenzamaximiliano@gmail.com>
2019-08-31 16:23:41 -0300

    feat(dwellingsTypes): show spinner if dwellingsTypes are loading

--- src/components/Charts/MonitoringGraphics/DwellingsTypes.js
+++ src/components/Charts/MonitoringGraphics/DwellingsTypes.js
@@ -1,53 +1,64 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 import {Bar} from 'react-chartjs-2';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faChartBar} from '@fortawesome/free-solid-svg-icons';
+import {faChartBar, faSpinner} from '@fortawesome/free-solid-svg-icons';
 import 'chartjs-plugin-datalabels';
 
 import {getTooltipLabel} from '../utils';
 
-const DwellingsTypes = ({dwellingsTypes}) => (
+const DwellingsTypes = ({dwellingsTypes, loadingDwellingsTypes}) => (
     <div className="box-doughnut">
         <h4 className="dwellings text-center">
             <FontAwesomeIcon icon={faChartBar}/>
             &nbsp;
             Tipo de viviendas
         </h4>
-        <Bar
-            data={dwellingsTypes}
-            height="100%"
-            options={{
-                tooltips: {
-                    callbacks: {
-                        title: () => '',
-                        label: getTooltipLabel
-                    }
-                },
-                legend: {
-                    display: false
-                },
-                plugins: {
-                    datalabels: {
-                        display: true,
-                        color: '#000',
-                        fontSize: 11,
-                        anchor: 'end',
-                        align: 'end',
-                        offset: -7
-                    }
-                },
-                backgroundColor: 'rgba(251, 85, 85, 0.4)',
-                showTooltips: false,
-                maintainAspectRatio: true,
-                responsive: true
-            }}
-        />
+        {loadingDwellingsTypes ? <FontAwesomeIcon icon={faSpinner} pulse size="3x"/> : (
+            <Bar
+                data={dwellingsTypes}
+                height="100%"
+                options={{
+                    tooltips: {
+                        callbacks: {
+                            title: () => '',
+                            label: getTooltipLabel
+                        }
+                    },
+                    legend: {
+                        display: false
+                    },
+                    plugins: {
+                        datalabels: {
+                            display: true,
+                            color: '#000',
+                            fontSize: 11,
+                            anchor: 'end',
+                            align: 'end',
+                            offset: -7
+                        }
+                    },
+                    backgroundColor: 'rgba(251, 85, 85, 0.4)',
+                    showTooltips: false,
+                    maintainAspectRatio: true,
+                    responsive: true
+                }}
+            />
+        )}
     </div>
 );
 
 DwellingsTypes.propTypes = {
-    dwellingsTypes: PropTypes.shape({}).isRequired
+    dwellingsTypes: PropTypes.shape({}),
+    loadingDwellingsTypes: PropTypes.bool
 };
 
-export default DwellingsTypes;
+DwellingsTypes.defaultProps = {
+    dwellingsTypes: {},
+    loadingDwellingsTypes: true
+};
+
+export default connect(state => ({
+    loadingDwellingsTypes: state.overview.loadingDwellingsTypes
+}))(DwellingsTypes);
