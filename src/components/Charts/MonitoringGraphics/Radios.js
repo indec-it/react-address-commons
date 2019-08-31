b8fd7fff93b392429aa2f89f20cbb4862ed8b31a
Maximiliano Forlenza <forlenzamaximiliano@gmail.com>
2019-08-31 16:25:28 -0300

    feat(radios): show spinner if radios are loading

--- src/components/Charts/MonitoringGraphics/Radios.js
+++ src/components/Charts/MonitoringGraphics/Radios.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 import {Row} from 'react-bootstrap';
 import {Doughnut} from 'react-chartjs-2';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faMapMarkerAlt} from '@fortawesome/free-solid-svg-icons';
+import {faMapMarkerAlt, faSpinner} from '@fortawesome/free-solid-svg-icons';
 
 import {getTooltipLabel} from '../utils';
 
-const Radios = ({provinceData, stateName}) => (
+const Radios = ({provinceData, stateName, loading}) => (
     <div className="box-doughnut">
         <h4 className="state text-center">
             <FontAwesomeIcon icon={faMapMarkerAlt}/>
@@ -16,34 +17,36 @@ const Radios = ({provinceData, stateName}) => (
             &nbsp;
             {stateName}
         </h4>
-        <Doughnut
-            data={provinceData}
-            height="280%"
-            options={{
-                tooltips: {
-                    callbacks: {
-                        label: getTooltipLabel
-                    }
-                },
-                legend: {
-                    display: false
-                },
-                plugins: {
-                    datalabels: {
+        {loading ? <FontAwesomeIcon icon={faSpinner} pulse size="3x"/> : (
+            <Doughnut
+                data={provinceData}
+                height="280%"
+                options={{
+                    tooltips: {
+                        callbacks: {
+                            label: getTooltipLabel
+                        }
+                    },
+                    legend: {
                         display: false
-                    }
-                },
-                elements: {
-                    center: {
-                        text: provinceData.total,
-                        fontStyle: 'Arial',
-                        sidePadding: 20
-                    }
-                },
-                maintainAspectRatio: true,
-                responsive: true
-            }}
-        />
+                    },
+                    plugins: {
+                        datalabels: {
+                            display: false
+                        }
+                    },
+                    elements: {
+                        center: {
+                            text: provinceData.total,
+                            fontStyle: 'Arial',
+                            sidePadding: 20
+                        }
+                    },
+                    maintainAspectRatio: true,
+                    responsive: true
+                }}
+            />
+        )}
         <Row>
             <ul className="legend">
                 <li>
@@ -87,11 +90,15 @@ Radios.propTypes = {
     provinceData: PropTypes.shape({
         total: PropTypes.number
     }).isRequired,
-    stateName: PropTypes.string
+    stateName: PropTypes.string,
+    loading: PropTypes.bool
 };
 
 Radios.defaultProps = {
-    stateName: null
+    stateName: null,
+    loading: true
 };
 
-export default Radios;
+export default connect(state => ({
+    loading: state.overview.loading
+}))(Radios);
