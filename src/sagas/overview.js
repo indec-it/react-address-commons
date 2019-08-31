ff61693887414fa1d6c50a24d00dedebdaff2a9a
Maximiliano Forlenza <forlenzamaximiliano@gmail.com>
2019-08-31 16:21:24 -0300

    refactor(sagas): using fork to non-blocking calls

--- src/sagas/overview.js
+++ src/sagas/overview.js
@@ -1,47 +1,97 @@
 /* eslint-disable prefer-destructuring */
-import {call, put, select} from 'redux-saga/effects';
+import {
+    call, put, select, fork
+} from 'redux-saga/effects';
 
 import {handleError, receiveOverview} from '../actions';
-import {receiveLogsByState} from '../actions/overview';
+import {
+    receiveLogsByState,
+    receiveBlocksForDashboard,
+    receiveSidesForDashboard,
+    receiveDwellingsForDashboard,
+    receiveDwellingsTypesForDashboard,
+    receiveLogsForOverview,
+    receiveUsersForOverview
+} from '../actions/overview';
 import MonitoringService from '../services/monitoring';
 import SyncTaskService from '../services/log';
 import UserService from '../services/user';
-import {getMonitoring} from '../util';
 
-export function* fetchOverview({profile}) {
+export function* fetchBlocksForOverview() {
+    try {
+        const {isNationalCoordinator, state} = yield select(({session}) => session.profile);
+        const blocks = yield call(MonitoringService.fetchBlocks, isNationalCoordinator ? {} : {state});
+        yield put(receiveBlocksForDashboard(blocks));
+    } catch (err) {
+        yield put(handleError(err));
+    }
+}
+
+export function* fetchSidesForOverview() {
+    try {
+        const {isNationalCoordinator, state} = yield select(({session}) => session.profile);
+        const sides = yield call(MonitoringService.fetchSides, isNationalCoordinator ? {} : {state});
+        yield put(receiveSidesForDashboard(sides));
+    } catch (err) {
+        yield put(handleError(err));
+    }
+}
+
+export function* fetchDwellingsForOverview() {
+    try {
+        const {isNationalCoordinator, state} = yield select(({session}) => session.profile);
+        const dwellings = yield call(MonitoringService.fetchDwellings, isNationalCoordinator ? {} : {state});
+        yield put(receiveDwellingsForDashboard(dwellings));
+    } catch (err) {
+        yield put(handleError(err));
+    }
+}
+
+export function* fetchDwellingsTypesForOverview() {
+    try {
+        const {isNationalCoordinator, state} = yield select(({session}) => session.profile);
+        const dwellingsTypes = yield call(
+            MonitoringService.fetchDwellingsTypes, isNationalCoordinator ? {} : {state}
+        );
+        yield put(receiveDwellingsTypesForDashboard(dwellingsTypes));
+    } catch (err) {
+        yield put(handleError(err));
+    }
+}
+
+export function* fetchLogsForOverview() {
+    try {
+        const {isNationalCoordinator, state} = yield select(({session}) => session.profile);
+        const logs = isNationalCoordinator ? yield call(SyncTaskService.fetchAllSyncTask) : yield call(
+            SyncTaskService.fetchSyncTaskByState, state
+        );
+        yield put(receiveLogsForOverview(logs));
+    } catch (err) {
+        yield put(handleError(err));
+    }
+}
+
+export function* fetchUsersForOverview() {
+    try {
+        const {isNationalCoordinator, state} = yield select(({session}) => session.profile);
+        const {users} = isNationalCoordinator ? yield call(UserService.fetchAll) : yield call(UserService.fetch, state);
+        yield put(receiveUsersForOverview(users));
+    } catch (err) {
+        yield put(handleError(err));
+    }
+}
+
+export function* fetchOverview() {
     try {
         const {isNationalCoordinator} = yield select(({session}) => session.profile);
         const {areas} = yield call(MonitoringService.fetchGeneralMonitoring);
-        let logs;
-        let users;
-        let dwellings;
-        let blocks;
-        let sides;
-        let dwellingsTypes;
-        if (isNationalCoordinator) {
-            users = yield call(UserService.fetchAll);
-            dwellings = yield call(MonitoringService.fetchDwellings);
-            blocks = yield call(MonitoringService.fetchBlocks);
-            sides = yield call(MonitoringService.fetchSides);
-            dwellingsTypes = yield call(MonitoringService.fetchDwellingsTypes);
-            logs = yield call(SyncTaskService.fetchAllSyncTask);
-        } else {
-            logs = yield call(SyncTaskService.fetchSyncTaskByState, profile.state);
-            users = (yield call(UserService.fetch, profile.state)).users;
-            dwellings = yield call(MonitoringService.fetchDwellings, {state: profile.state});
-            blocks = yield call(MonitoringService.fetchBlocks, {state: profile.state});
-            sides = yield call(MonitoringService.fetchSides, {state: profile.state});
-            dwellingsTypes = yield call(MonitoringService.fetchDwellingsTypes, {state: profile.state});
-        }
-        const response = getMonitoring(dwellings, blocks, sides, dwellingsTypes);
-        yield put(receiveOverview(
-            areas,
-            response,
-            logs,
-            users,
-            areas.map(area => area._id.state),
-            isNationalCoordinator
-        ));
+        yield fork(fetchBlocksForOverview);
+        yield fork(fetchSidesForOverview);
+        yield fork(fetchDwellingsForOverview);
+        yield fork(fetchDwellingsTypesForOverview);
+        yield fork(fetchLogsForOverview);
+        yield fork(fetchUsersForOverview);
+        yield put(receiveOverview(areas, areas.map(area => area._id.state), isNationalCoordinator));
     } catch (err) {
         yield put(handleError(err));
     }
